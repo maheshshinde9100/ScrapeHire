78c28ce4e28f85541d709dfaae4ba6d931eaee2f
maheshshinde9100 <maheshshinde9100@gmail.com>
2026-01-13 21:47:09 +0530

    correctly using datetime module

--- server/app/schemas/job.py
+++ server/app/schemas/job.py
@@ -1,5 +1,6 @@
 from pydantic import BaseModel
 from typing import Optional
+from datetime import datetime
 
 
 class JobBase(BaseModel):
@@ -15,7 +16,7 @@ class JobCreate(JobBase):
 
 class JobRead(JobBase):
     id: int
-    created_at: Optional[str]
+    created_at: Optional[datetime]
 
     class Config:
         from_attributes = True
