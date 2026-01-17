4c09907a1e9fa062dffdc05b64a194ce3ed8c1ef
maheshshinde9100 <maheshshinde9100@gmail.com>
2026-01-17 21:35:52 +0530

    remoteok

--- server/app/scraping/remoteok.py
+++ server/app/scraping/remoteok.py
@@ -12,8 +12,13 @@ def scrape_remoteok():
         title = job.select_one("h2")
         company = job.select_one("h3")
         if title and company:
+            url_suffix = job.get("data-href")
+            full_url = f"https://remoteok.com{url_suffix}" if url_suffix else None
+            
             jobs.append({
                 "title": title.text.strip(),
-                "company": company.text.strip()
+                "company": company.text.strip(),
+                "url": full_url,
+                "description": "" # RemoteOK doesn't easily expose desc in the list view
             })
     return jobs
